30fb18dac1d5a7f7102d9ab9f9aa0cc71f0aef69
Brian Schmiedt <SA_Brian.Schmiedt@fglife.com>
2025-06-05 11:45:46 -0500

    Adding more dockers

--- helpers/helpers.py
+++
@@ -1,7 +0,0 @@
-from pytest_bdd import scenarios, given, when, then, parsers
-from selenium.webdriver.common.by import By
-
-
-def wait_for_asdf(self, xpath):
-    self._driver_wait.until(EC.element_to_be_clickable((By.XPATH, f"{xpath}")))
-    return self._driver.find_element(By.XPATH, xpath)
